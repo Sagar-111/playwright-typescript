2f79f3244df19fe50df0667b1bd66e67d624e5fe
Sagar Choudhari <147383031+Sagar-111@users.noreply.github.com>
2026-04-01 17:06:01 +0530

    chore: Initial scaffolding for the framework (#2)

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  timeout: 30000,
+  retries: 1,
+  reporter: [['html', { outputFolder: 'playwright-report', open: 'never' }]],
+  use: {
+    baseURL: 'https://the-internet.herokuapp.com',
+    headless: true,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+});
