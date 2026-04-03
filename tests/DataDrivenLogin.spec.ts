c4874ab4448193523e446c75a051773709f61791
Sagar <sgrchdhr82@gmail.com>
2026-04-03 22:31:31 +0530

    feat: externalize test data to JSON fixtures

---
+++ tests/DataDrivenLogin.spec.ts
@@ -0,0 +1,26 @@
+import { test, expect } from '@playwright/test'
+import { LoginPage } from '../pages/LoginPage'
+import testData from '../fixtures/loginTestData.json'
+
+const users = [
+    { ...testData.validUser, expectSuccess: true },
+    { ...testData.invalidUser, expectSuccess: false },
+    { ...testData.lockedUser, expectSuccess: false }
+]
+
+test.describe('Login test Using Data from json', () => {
+    for (const user of users) {
+        test(`login test for ${user.username}`, async ({ page }) => {
+            const loginPage = new LoginPage(page);
+            await loginPage.navigate();
+            await loginPage.login(user.username, user.password);
+
+            if (user.expectSuccess) {
+                await expect(page).toHaveTitle('Swag Labs');
+                await expect(page).toHaveURL('/inventory.html');
+            } else {
+                await expect(loginPage.loginErrorMessage).toBeVisible();
+            }
+        });
+    }
+});
